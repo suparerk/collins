2c91cf3e482ea34aff940e5b6da2c33062eecaaf
Ratchaneekorn Suwanwong <r.suwanwong@gmail.com>
2018-10-10 16:13:30 +0700

    update ui layout

--- ui-dist/js/scripts.js
+++ ui-dist/js/scripts.js
@@ -3,7 +3,34 @@
 //=============================================
 $("document").ready(function() {
 
-    
+    // Custom Scrollbar
+    // if ($(window).outerWidth() > 768) {
+    //     $("nav.side-navbar").mCustomScrollbar({
+    //         scrollInertia: 200
+    //     });
+    // }
+
+    // Side Navbar Functionality
+    $('#toggle-btn').on('click', function (e) {
+
+        e.preventDefault();
+
+        if ($(window).outerWidth() > 768) {
+            $('nav.side-navbar').toggleClass('shrink');
+            $('.page').toggleClass('active');
+        } else {
+            $('nav.side-navbar').toggleClass('show-sm');
+            $('.page').toggleClass('active-sm');
+        }
+    });
+
+    // External links to new window
+    $('.external').on('click', function (e) {
+
+        e.preventDefault();
+        window.open($(this).attr("href"));
+    });
+
     resize();
     
 });//document.ready
