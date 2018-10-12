54af97cbffaa389ac88492c2fd60868b731ab3e0
Suparerk <ch.suparerk@gmail.com>
2018-10-12 11:17:19 +0700

    Dashboard with style

--- components/menu.js
+++ components/menu.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Head from 'next/head'
+import '../css/style.css'
+import '../css/login.css'
 
 const Menu = () => (
   <div>
@@ -8,7 +10,47 @@ const Menu = () => (
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" />
     </Head>
-    <div className="page">
+    <nav className="side-navbar shrink">
+      <div className="side-navbar-wrapper">
+        <div className="sidenav-header d-flex align-items-center">
+          <div className="sidenav-header-inner">
+            <div className="sidenav-header-inner--wrap">
+              <div id="toggle-btn" className="toggle-btn">
+                <div className="logo-wrap">
+                  <img src="/static/images/logo/logo.png" />
+                </div>
+              </div>
+              <a className="nav-link" href="/static/images/dashboard.html">
+                <span>Dashboard</span>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="main-menu">
+          <ul id="side-main-menu" className="side-menu list-unstyled">
+            <div>
+              <li><a className="nav-link active" href="po.html"><span><i className="icon icon-PO"></i></span>Purchase Order</a></li>
+              <li><a className="nav-link" href="gr.html"><span><i className="icon icon-GR"></i></span>Goods Receive</a></li>
+              <li><a className="nav-link" href="invoice.html"><span><i className="icon icon-INV"></i></span>Invoice</a></li>
+              <li><a className="nav-link" href="cn.html"><span><i className="icon icon-CN"></i></span>Credit Note</a></li>
+              <li><a className="nav-link" href="dn.html"><span><i className="icon icon-DN"></i></span>Debit Note</a></li>
+              <li><a className="nav-link" href="3wm.html"><span><i className="icon icon-3wm"></i></span>3 Way Matching</a></li>
+              <li><a className="nav-link" href="2wm.html"><span><i className="icon icon-2wm"></i></span>2 Way Matching</a></li>
+              <li><a className="nav-link" href="doa.html"><span><i className="icon icon-DOA"></i></span>Waiting DOA Approval</a></li>
+            </div>
+            <li className="bottom"><a className="nav-link" href="login.html"><span><i className="icon icon-Logout"></i></span>Logout</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <div className="sidenav-header-toggle">
+      <div id="toggle-btn-open" className="toggle-btn-open">
+        <div className="logo-wrap">
+          <img src="/static/images/logo/logo.png" />
+        </div>
+      </div>
+    </div>
+    <div className="page active">
       <div className="container main pt-4">
         <div className="container">
           <div className="row">
@@ -31,7 +73,7 @@ const Menu = () => (
               <a className="btn w-100" href="dashboard.html">Dashboard</a>
             </div>
           </div>
-          <div className="row mb-5" />
+          <div className="row mb-5"></div>
         </div>
       </div>
     </div>
