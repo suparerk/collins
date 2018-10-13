e819daed84a4e92b45133001f165fc0a69bf9803
Ratchaneekorn Suwanwong <r.suwanwong@gmail.com>
2018-10-13 22:17:14 +0700

    update html login and invoice list

--- ui-dist/js/scripts.js
+++ ui-dist/js/scripts.js
@@ -8,16 +8,6 @@ $("document").ready(function() {
         $(this).toggleClass("active");
     });
 
-    // $('.selectpicker').selectpicker();
-    // $('.datepicker').datepicker();
-
-    // Custom Scrollbar
-    // if ($(window).outerWidth() > 768) {
-    //     $("nav.side-navbar").mCustomScrollbar({
-    //         scrollInertia: 200
-    //     });
-    // }
-
     // Side Navbar Functionality
     if ($(window).outerWidth() > 768) {
     $('nav.side-navbar').hover( 
@@ -31,6 +21,7 @@ $("document").ready(function() {
             } 
         );
     }
+
     // $('#toggle-btn, #toggle-btn-open').on('click', function (e) {
     //     e.preventDefault();
     //     if ($(window).outerWidth() > 768) {
@@ -73,3 +64,51 @@ $(window).on('load', function(){
 
 
 }); //windows.load
+
+
+
+//=============================================
+//    Add/Remove column datatable display
+//=============================================
+// add all column to current list
+$('body').on('click', 'button.add-all', function(){
+    $(this).removeClass('add').addClass('disabled');
+    var element = $('#allColumn').each(function(){
+        var li_element = $(this).html();
+        $('#currentColumn').append(li_element);
+    });
+    $('#allColumn').find('.li').remove();
+    $('#currentColumn').find('.li button').removeClass('add').addClass('remove');
+});
+
+// remove all column to current list
+$('body').on('click', 'button.remove-all', function(){
+    // $(this).removeClass('remove').addClass('disabled');
+    var element = $('#currentColumn').each(function(){
+        var li_element = $(this).html();
+        $('#allColumn').append(li_element);
+        $('#allColumn').find('.li.fixed').remove();
+    });
+    $('#currentColumn').find('.li:not(".fixed")').remove();
+    $('#allColumn').find('.li button').removeClass('remove').addClass('add');
+});
+
+// add column current list
+$('body').on('click', '#allColumn button', function(){
+    $(this).removeClass('add').addClass('remove');
+    var element = $(this).parents('.li').html();
+    var target = $('#currentColumn');
+
+    target.append('<li class="li">'+element+'</li>');
+    $(this).parents('.li').remove();
+});
+
+// remove column current list
+$('body').on('click', '#currentColumn .li:not(".fixed") button', function(){
+    $(this).removeClass('remove').addClass('add');
+    var element = $(this).parents('.li').html();
+    var target = $('#allColumn');
+
+    target.append('<li class="li">'+element+'</li>');
+    $(this).parents('.li').remove();
+});
